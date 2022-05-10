1133398d557f69cf62e671e33280582409f782a4
zack stevens <zack.st7@gmail.com>
2022-05-10 11:29:49 -0600

    Troubleshooting

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 /**
  * @param {import('probot').Probot} app
- * @param {import('probot-settings')} settings
  */
-module.exports = (app, settings,) => {
+module.exports = (app) => {
     const SettingsApp = require('probot-settings/lib/settings')
+    const settings = require('probot-settings')
     app.log("Yay! The app was loaded!");
     settings.default(app, null, Settings = SettingsApp)
     app.log("Yay! Settings ran maybe!")
