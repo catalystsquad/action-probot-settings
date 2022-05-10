f62483ae6ad5405420c06b72abcf94467004b09e
zack stevens <zack.st7@gmail.com>
2022-05-10 12:03:16 -0600

    Troubleshooting

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
+
 /**
- * @param {import('probot').Probot} app
+ * @param {import('probot').Probot} robot
  */
-module.exports = (app) => {
-    const SettingsApp = require('probot-settings/lib/settings')
-    const settings = require('probot-settings')
-    app.log.info(`log level: ${app.log.level}`)
-    app.log.level = 'debug'
-    app.log.info(`log level: ${app.log.level}`)
-    app.log.info("Yay! The app was loaded!");
-    app.log.debug("testing debug logging")
-    settings.default(app, null, Settings = SettingsApp)
-    app.log.error("Yay! Settings ran maybe!")
+module.exports = (robot) => {
+    const mergeArrayByName = require("probot-settings/lib/mergeArrayByName");
+    const Settings = require("probot-settings/lib/settings");
+    async function syncSettings (context, repo = context.repo()) {
+        const config = await context.config('settings.yml', {}, { arrayMerge: mergeArrayByName })
+        return Settings.sync(context.octokit, repo, config)
+    }
+
+    robot.on('push', async context => {
+        const { payload } = context
+        const { repository } = payload
+
+        const defaultBranch = payload.ref === 'refs/heads/' + repository.default_branch
+        if (!defaultBranch) {
+            robot.log.debug('Not working on the default branch, returning...')
+            return
+        }
+
+        const settingsModified = payload.commits.find(commit => {
+            return commit.added.includes(Settings.FILE_NAME) || commit.modified.includes(Settings.FILE_NAME)
+        })
+
+        if (!settingsModified) {
+            robot.log.debug(`No changes in '${Settings.FILE_NAME}' detected, returning...`)
+            return
+        }
+
+        return syncSettings(context)
+    })
+
+    robot.on('repository.edited', async context => {
+        const { payload } = context
+        const { changes, repository } = payload
+
+        if (!Object.prototype.hasOwnProperty.call(changes, 'default_branch')) {
+            robot.log.debug('Repository configuration was edited but the default branch was not affected, returning...')
+            return
+        }
+
+        robot.log.debug(`Default branch changed from '${changes.default_branch.from}' to '${repository.default_branch}'`)
+
+        return syncSettings(context)
+    })
+
+    robot.on('repository.created', async context => {
+        return syncSettings(context)
+    })
 };
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const { run } = require("@probot/adapter-github-actions");
 const app = require("./app");
-console.log(process.env)
-process.env.LOG_LEVEL='debug'
 
 run(app);
