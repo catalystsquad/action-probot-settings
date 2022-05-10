d4483247b2d450e0a23e5046fb908aab257b31b9
zack stevens <zack.st7@gmail.com>
2022-05-10 11:33:51 -0600

    Troubleshooting

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
 module.exports = (app) => {
     const SettingsApp = require('probot-settings/lib/settings')
     const settings = require('probot-settings')
-    app.log("Yay! The app was loaded!");
+    app.log.info("Yay! The app was loaded!");
+    app.log.debug("testing debug logging")
     settings.default(app, null, Settings = SettingsApp)
-    app.log("Yay! Settings ran maybe!")
+    app.log.error("Yay! Settings ran maybe!")
 };
