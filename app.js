d9acca1c97ad998a8383a2afdbce8010d735c808
zack stevens <zack.st7@gmail.com>
2022-05-10 11:16:16 -0600

    Troubleshooting

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
+const settings = require('probot-settings')
+import SettingsApp from 'probot-settings/lib/settings'
 /**
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
     app.log("Yay! The app was loaded!");
-
-    app.on("issues.opened", async (context) => {
-        return context.octokit.issues.createComment(
-            context.issue({ body: "Hello, World!" })
-        );
-    });
+    settings.default(app, _, SettingsApp)
+    app.log("Yay! Settings ran maybe!")
 };
